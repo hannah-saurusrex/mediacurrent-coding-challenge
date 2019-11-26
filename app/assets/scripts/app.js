18ff9998a60a83e9799d40eb9eeb947742020bf3
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-11-26 15:45:32 -0700

    styled all font and button elements for dark mode toggle

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -8,3 +8,26 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+// function classToggle() {
+//     this.classList.toggle('.light-mode');
+//     this.classList.toggle('.dark-mode');
+// }
+
+// document.getElementById('theme').addEventListener('click', classToggle);
+
+// document.getElementById("theme").addEventListener('click', function(e) {
+//     (event.target.checked) ? document.html.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+// });
+
+// const toggleSwitch = document.querySelector('.theme-change input[type="checkbox"');
+
+// function switchTheme(e) {
+//     if (e.target.checked) {
+//         document.documentElement.setAttribute('data-theme', 'dark');
+//     } else {
+//         document.documentElement.setAttribute('data-theme', 'light');
+//     }
+// }
+
+// toggleSwitch.addEventListener('change', switchTheme, false);
