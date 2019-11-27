317e8e3564684bf29a69f2aeac0fb733bdbdc352
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-11-27 11:27:58 -0700

    cleaned up css

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,10 +1,10 @@
 function darkMode() {
     var x = document.getElementById("dark-mode");
     if (x.innerHTML === "Dark Mode") {
-        x.innerHTML = "Light Mode";
+        x.innerHTML = "Light Mode"; // change text to read Light Mode on click
     } else {
         x.innerHTML = "Dark Mode";
     }
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    element.classList.toggle("dark-mode"); // on click, toggle on the Dark Mode class styles
 }
